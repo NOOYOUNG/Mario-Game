3532dea3af2370dd2497b3467b482c6cce594daf
NOOYOUNG <chyooy23@gmail.com>
2024-06-05 16:43:01 +0900

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -39,11 +39,11 @@ function moveRight() {
     mv += 50;
 
     if (mv >= windowWidth) {
-        // Reset Mario's position to the beginning when he reaches the end of the screen
+        // 마리오가 화면 끝에 도달했을 때 위치를 처음으로 재설정
         mv = 0;
         mario.css({ left: mv + 'px' });
     } else {
-        // Move Mario to the right
+        // 마리오를 오른쪽으로 이동
         mario.animate({
             left: mv + 'px'
         }, 100);
@@ -54,8 +54,8 @@ function moveMushroom() {
     var mushroom = $("#mushroom");
     var windowWidth = $(window).width();
 
-    // Start the continuous movement
-    mushroom.css({ left: windowWidth + 'px' });  // Reset the position to the right side
+    // 연속적인 움직임을 시작
+    mushroom.css({ left: windowWidth + 'px' });  // 위치를 오른쪽으로 재설정
     animateMushroom();
 }
 
@@ -67,7 +67,7 @@ function animateMushroom() {
     mushroom.animate({
         left: '-=50px'
     }, 100, function() {
-        // 마리오와의 충돌을 확인합니다
+        // 마리오와의 충돌을 확인
         var mushroomPos = mushroom.position().left;
         var marioPos = mario.position().left;
         var marioTop = mario.position().top;
@@ -75,25 +75,25 @@ function animateMushroom() {
         if (mushroomPos <= marioPos + mario.width() && mushroomPos + mushroom.width() >= marioPos) {
             if (marioTop >= 410) {
                 // 마리오가 땅에 있을 때 충돌 감지
-                life -= 1; // 생명을 하나 감소시킵니다
-                $("#lifeDisplay").text(life); // 목숨을 화면에 업데이트합니다
+                life -= 1; // 생명을 하나 감소
+                $("#lifeDisplay").text(life); // 목숨을 화면에 업데이트
                 if (life <= 0) {
                     // 생명이 0이 되었을 때 게임 오버
-                    gameOver.show(); // 게임 오버 이미지를 표시합니다
+                    gameOver.show(); // 게임 오버 이미지를 표시
                     mario.hide();
                     mushroom.hide();
-                    return; // 게임 오버이므로 더 이상 애니메이션을 계속하지 않습니다
+                    return; // 게임 오버이므로 더 이상 애니메이션을 계속하지 않음
                 }
-                mushroom.css({ left: $(window).width() + 'px' }); // 버섯을 오른쪽 끝으로 재설정합니다
+                mushroom.css({ left: $(window).width() + 'px' }); // 버섯을 오른쪽 끝으로 재설정
             }
         } else if (mushroom.position().left <= -mushroom.width()) {
-            // 버섯이 화면 왼쪽 끝으로 이동하면 위치를 오른쪽으로 재설정하고 점수를 증가시킵니다
+            // 버섯이 화면 왼쪽 끝으로 이동하면 위치를 오른쪽으로 재설정하고 점수를 증가
             mushroom.css({ left: $(window).width() + 'px' });
-            score += 1; // 점수를 하나 증가시킵니다
-            $("#scoreDisplay").text(score); // 점수를 화면에 표시합니다
+            score += 1; // 점수를 하나 증가
+            $("#scoreDisplay").text(score); // 점수를 화면에 표시
         }
 
-        // 이동을 계속합니다
+        // 계속 이동
         animateMushroom();
     });
 }
